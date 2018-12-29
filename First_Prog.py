ca788d25a308beb3c1458a9c17dfd552affc89e5
HarshadRepo <harshadbhoite1@gmail.com>
2018-12-29 12:41:00 +0530

    Added student id and name function

--- First_Prog.py
+++ First_Prog.py
@@ -1,3 +1,18 @@
-print("My First Java Program")
-print("My Second Python Program")
-print("My Third JavaScript Program")
+class student():
+    id = ""
+    name = ""
+
+    def __init__(self):
+        pass
+
+    def get_Id(self):
+        return id
+
+    def set_id(self, id):
+        self.id = id
+
+    def get_name(self):
+        return self.name
+
+    def set-name(self):
+        self.name = name
