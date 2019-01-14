fbdb2ac540d81d3caf0d226ab65bc88ae47a5de4
Harshad <harshadbhoite619@gmail.com>
2019-01-14 21:45:51 +0530

    Added DevForth.py file

---
+++ dev/DevForth.py
@@ -0,0 +1,30 @@
+class DevForth:
+""" My First Python Program """
+
+    marks = ""
+    id = ""
+    student_name = ""
+
+    def __init__(self):
+        pass
+
+    def marks(self):
+        return id
+
+    def get_marks(self, id):
+        self.marks = id
+
+    def get_Id(self):
+        return id
+	
+	def set_Id(self, id):
+        self.id = id
+		
+    def set_id(self, id):
+        self.id = id
+
+    def get_name(self):
+        return self.student_name
+
+    def set_name(self, name):
+        self.student_name = name
